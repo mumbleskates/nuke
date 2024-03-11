eb9ab33f2be20c59b5b472b0d87cf79e88b566ed
Kent Ross <k@mad.cash>
2024-03-10 19:02:10 -0700

    wip

--- safe_arena.go
+++ safe_arena.go
@@ -12,17 +12,38 @@ const (
 	intAlignment = unsafe.Alignof(int(0))
 )
 
+// An arena type that can retain strong pointers from data that is stored inside
+// it.
 type safeArena struct {
 	// Plain Old Data buffers, with alignments 1, 2, 4, 8, and everything else.
 	// The index of the group used comes from alignmentGroupIndex
-	podSlabs   safeSlabGroup
+	podSlabs safeSlabGroup
+	// Slabs that hold arbitrary types, one slab group for each type. These
+	// groups will hold *only* values of that type, and the underlying buffers
+	// are created as slices of that type so they will be recognized by the GC.
 	typedSlabs map[reflect.Type]*safeSlabGroup
-	// TODO(widders): ownership and growth
+	// Number of slots to create typed slab groups with for each new type added.
+	initialTypedSlots int
+}
+
+func makeSafeArena() *safeArena {
+	return makeSafeArenaWithOptions(4096, 64)
+}
+
+func makeSafeArenaWithOptions(initialBytes int, initialTypedSlots int) *safeArena {
+	return &safeArena{
+		podSlabs: safeSlabGroup{
+			bufs: []safeSlab{makeSafeSlab[byte](initialBytes)},
+		},
+		initialTypedSlots: initialTypedSlots,
+	}
 }
 
 func newInSafeArena[T any](arena *safeArena) *T {
 	var group *safeSlabGroup
 	var size, align uintptr
+	// We switch on the type of T, allowing us to put known common POD types
+	// into the POD slabs instead of in typed slabs.
 	switch any((*T)(nil)).(type) {
 	case *byte, *int8, *bool:
 		size, align = 1, 1
@@ -52,8 +73,9 @@ func newInSafeArena[T any](arena *safeArena) *T {
 		tType := reflect.TypeFor[T]()
 		group = arena.typedSlabs[tType]
 		if group == nil {
-			group = &safeSlabGroup{}
-			arena.typedSlabs[tType] = group
+			newGroup := makeSafeSlabGroup[T](arena.initialTypedSlots)
+			arena.typedSlabs[tType] = &newGroup
+			group = &newGroup
 		}
 		// Typed slabs don't require extra work to align them, as the slab was
 		// allocated as a slice of T. It is therefore already guaranteed to be
@@ -62,6 +84,7 @@ func newInSafeArena[T any](arena *safeArena) *T {
 		return (*T)(group.getFast(tType.Size()))
 	}
 	return (*T)(group.get(size, align))
+	// TODO(widders): actually we might need to add a slab here
 }
 
 // Makes space for any type in the arena, with a user-guarantee that the type
@@ -86,6 +109,13 @@ type safeSlabGroup struct {
 	bufs []safeSlab
 	// Index of the first slab that hasn't become full
 	firstWithFreeSpace int
+}
+
+func makeSafeSlabGroup[T any](initialSlots int) safeSlabGroup {
+	return safeSlabGroup{
+		bufs:               []safeSlab{makeSafeSlab[T](initialSlots)},
+		firstWithFreeSpace: 0,
+	}
 }
 
 func (sg *safeSlabGroup) getFast(size uintptr) unsafe.Pointer {
@@ -115,34 +145,14 @@ type safeSlab struct {
 	offset uintptr
 }
 
-func makeSafeSlab(size int, alignment uintptr) safeSlab {
-	// We take ownership of an allocated byte buffer and only keep its head
-	// pointer. We could keep the slice itself, but we want to track the offset
-	// of the next allocation, not re-slice the front of the slice. If we slice
-	// off the front of the buffer, we can't get it offset, so effectively one of
-	// the words of the []byte slice would be wasted.
-	slab := safeSlab{
-		buf:    unsafe.Pointer(&make([]byte, size)[0]),
-		size:   size,
-		offset: 0,
-	}
-	return slab
-}
-
-func makeTypedSafeSlab[T any](size int) safeSlab {
-	// We want to allocate the smallest slice that is at least `size` bytes
-	typeSize := int(reflect.TypeFor[T]().Size())
-	targetSlots := size / typeSize
-	if typeSize*targetSlots < size {
-		targetSlots++
-	}
-	actualSize := typeSize * targetSlots
+func makeSafeSlab[T any](slots int) safeSlab {
+	var t T
 	return safeSlab{
 		// We can still retain ownership of the buffer by punning it as another
 		// type, and we can still bump-allocate the precise typed slots of the
 		// slice if we only ever put that specific type in it.
-		buf:    unsafe.Pointer(&make([]T, targetSlots)[0]),
-		size:   actualSize,
+		buf:    unsafe.Pointer(&make([]T, slots)[0]),
+		size:   slots * int(unsafe.Sizeof(t)),
 		offset: 0,
 	}
 }
