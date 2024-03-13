da215eff120c5292a1aa7dc71633028aef74195b
Kent Ross <k@mad.cash>
2024-03-13 05:46:22 -0700

    looking much better

--- arena.go
+++ arena.go
@@ -3,6 +3,7 @@
 package nuke
 
 import (
+	"reflect"
 	"unsafe"
 )
 
@@ -10,37 +11,47 @@ import (
 type Arena interface {
 	// Reset resets the arena's state.
 	Reset()
+
+	getTyped(reflect.Type, int) unsafe.Pointer
+	getPOD(reflect.Type, int) unsafe.Pointer
 }
 
 // New allocates memory for a value of type T using the provided Arena.
-// If the arena is non-nil, it returns a  *T pointer with memory allocated from the arena.
-// If passed arena is nil, it allocates memory using Go's built-in new function.
-func New[T any](a Arena) *T {
-	if a != nil {
-		var x T
-		if ptr := make(unsafe.Sizeof(x), unsafe.Alignof(x)); ptr != nil {
-			return (*T)(ptr)
-		}
+// If the arena is non-nil, it returns a  *T pointer with memory allocated from
+// the arena. If passed arena is nil, it allocates memory using Go's built-in
+// new function.
+func New[T any](arena Arena) *T {
+	if arena != nil {
+		return &Make[T](arena, 1)[0]
 	}
 	return new(T)
 }
 
-// TODO(widders): public apis are the only generic part. they have to do the
-//  real allocations themselves in those functions, and the arena types have to
-//  support all of those actions via private interface methods.
+func NewPOD[T any](arena Arena) *T {
+	if arena != nil {
+		return &MakePOD[T](arena, 1)[0]
+	}
+	return new(T)
+}
 
 // MakeSlice creates a slice of type T with a given length and capacity,
 // using the provided Arena for memory allocation.
 // If the arena is non-nil, it returns a slice with memory allocated from the arena.
 // Otherwise, it returns a slice using Go's built-in make function.
-func MakeSlice[T any](a Arena, len, cap int) []T {
-	if a != nil {
-		var x T
-		bufSize := int(unsafe.Sizeof(x)) * cap
-		if ptr := (*T)(a.Alloc(uintptr(bufSize), unsafe.Alignof(x))); ptr != nil {
-			s := unsafe.Slice(ptr, cap)
-			return s[:len]
-		}
+func Make[T any](arena Arena, n int) []T {
+	if arena != nil {
+		ptr := (*T)(arena.getTyped(reflect.TypeFor[T](), n))
+		return unsafe.Slice(ptr, n)
 	}
-	return make([]T, len, cap)
+	return make([]T, n)
+}
+
+// Make space for any type in the arena, with a user-declared guarantee that
+// the type contains no pointers.
+func MakePOD[T any](arena Arena, n int) []T {
+	if arena != nil {
+		ptr := (*T)(arena.getPOD(reflect.TypeFor[T](), n))
+		return unsafe.Slice(ptr, n)
+	}
+	return make([]T, n)
 }
--- safe_arena.go
+++ safe_arena.go
@@ -5,12 +5,50 @@ import (
 	"unsafe"
 )
 
-const (
-	ptrSize      = unsafe.Sizeof((*byte)(nil))
-	ptrAlignment = unsafe.Alignof((*byte)(nil))
-	intSize      = unsafe.Sizeof(int(0))
-	intAlignment = unsafe.Alignof(int(0))
+var (
+	byteType = reflect.TypeFor[byte]()
 )
+
+// TODO(widders): recursive assert check traversing a type with reflect to make
+//  sure it is POD for users to use
+
+type Options struct {
+	InitialBytes      int
+	InitialTypedSlots int
+}
+
+func MakeSafeArena() Arena {
+	return MakeSafeArenaWithOptions(Options{
+		InitialBytes:      4096,
+		InitialTypedSlots: 64,
+	})
+}
+
+func MakeSafeArenaWithOptions(options Options) Arena {
+	return &safeArena{
+		podSlabs: safeSlabGroup{
+			slabs: []safeSlab{makeSafeSlab(byteType, options.InitialBytes)},
+		},
+		initialTypedSlots: options.InitialTypedSlots,
+	}
+}
+
+// Gets basic layout information for T. This includes its size and alignment,
+// whether it is guaranteed to be a POD type (Plain Old Data with no pointers),
+// and will also include the reflected type of T if it may be needed.
+func isPOD(ty reflect.Type) bool {
+	switch ty.Kind() {
+	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8,
+		reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
+		return true
+	default:
+		return false
+	}
+}
+
+// TODO(widders): Sprintf
 
 // An arena type that can retain strong pointers from data that is stored inside
 // it.
@@ -25,130 +63,6 @@ type safeArena struct {
 	initialTypedSlots int
 }
 
-type Options struct {
-	InitialBytes      int
-	InitialTypedSlots int
-}
-
-func MakeArena() Arena {
-	return MakeArenaWithOptions(Options{
-		InitialBytes:      4096,
-		InitialTypedSlots: 64,
-	})
-}
-
-func MakeArenaWithOptions(options Options) Arena {
-	return &safeArena{
-		podSlabs: safeSlabGroup{
-			slabs: []safeSlab{makeSafeSlab[byte](options.InitialBytes)},
-		},
-		initialTypedSlots: options.InitialTypedSlots,
-	}
-}
-
-// Simple layout information for a given type
-type layout struct {
-	size  uintptr
-	align uintptr
-	isPOD bool
-	ty    reflect.Type
-}
-
-// Gets basic layout information for T. This includes its size and alignment,
-// whether it is guaranteed to be a POD type (Plain Old Data with no pointers),
-// and will also include the reflected type of T if it may be needed.
-func getLayout[T any]() layout {
-	var size, align uintptr
-	var mayContainPointer bool
-	var tType reflect.Type
-
-	switch any((*T)(nil)).(type) {
-	case *byte, *int8, *bool:
-		size, align = 1, 1
-	case *int16, *uint16:
-		size, align = 2, 2
-	case *int32, *uint32, *float32:
-		size, align = 4, 4
-	case *int64, *uint64, *float64:
-		size, align = 8, 8
-	case *int, *uint:
-		size, align = intSize, intAlignment
-	case *uintptr:
-		size, align = ptrSize, ptrAlignment
-	case *complex64, *complex128:
-		tType = reflect.TypeFor[T]()
-		size, align = tType.Size(), uintptr(tType.Align())
-	default:
-		tType = reflect.TypeFor[T]()
-		size, align = tType.Size(), uintptr(tType.Align())
-		switch tType.Kind() {
-		case reflect.Struct, reflect.Pointer, reflect.String, reflect.Slice,
-			reflect.Map, reflect.Interface:
-			mayContainPointer = true
-		case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16,
-			reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8,
-			reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Float32,
-			reflect.Float64, reflect.Complex64, reflect.Complex128:
-			mayContainPointer = false
-		default:
-			mayContainPointer = true
-		}
-	}
-
-	return layout{
-		size:  size,
-		align: align,
-		isPOD: !mayContainPointer,
-		ty:    tType,
-	}
-}
-
-func getPODLayout[T any]() layout {
-	var t T
-	return layout{
-		size:  unsafe.Sizeof(t),
-		align: unsafe.Alignof(t),
-		isPOD: true,
-	}
-}
-
-func newInSafeArena[T any](arena *safeArena, n int) []T {
-	var ptr *T
-	tLayout := getLayout[T]()
-	if tLayout.isPOD {
-		ptr = (*T)(arena.podSlabs.newPODSlice(tLayout, n))
-	} else {
-		// When inserting typed data, we have to manage growing the slab groups
-		// directly to ensure that the slabs will be allocated with the correct
-		// type information.
-		group := arena.typedSlabs[tLayout.ty]
-		if group == nil {
-			newGroup := makeSafeSlabGroup[T](arena.initialTypedSlots)
-			arena.typedSlabs[tLayout.ty] = &newGroup
-			group = &newGroup
-		}
-		ptr = (*T)(group.get(tLayout.size, tLayout.align))
-		if ptr == nil {
-			growSlabGroup[T](group, tLayout.size, n)
-			ptr = (*T)(group.getFast(tLayout.size))
-			if ptr == nil {
-				// This should never happen
-				panic("slab allocation failed!")
-			}
-		}
-	}
-	return unsafe.Slice(ptr, n)
-}
-
-// Make space for any type in the arena, with a user-declared guarantee that
-// the type contains no pointers.
-func newPODInSafeArena[T any](arena *safeArena, n int) []T {
-	ptr := (*T)(arena.podSlabs.newPODSlice(getPODLayout[T](), n))
-	return unsafe.Slice(ptr, n)
-}
-
-// TODO(widders): Sprintf
-
 // Reset the arena, erasing all data written to it. This will gradually
 // shrink the arena's owned memory if much of its capacity was unused.
 func (sa *safeArena) Reset() {
@@ -156,6 +70,27 @@ func (sa *safeArena) Reset() {
 	for _, slab := range sa.typedSlabs {
 		slab.reset()
 	}
+}
+
+func (sa *safeArena) getTyped(ty reflect.Type, n int) unsafe.Pointer {
+	if isPOD(ty) {
+		return sa.podSlabs.getPOD(ty, n)
+	} else {
+		// When inserting typed data, we have to manage growing the slab groups
+		// directly to ensure that the slabs will be allocated with the correct
+		// type information.
+		group := sa.typedSlabs[ty]
+		if group == nil {
+			newGroup := makeSafeSlabGroup(ty, sa.initialTypedSlots)
+			sa.typedSlabs[ty] = &newGroup
+			group = &newGroup
+		}
+		return group.getTyped(ty, n)
+	}
+}
+
+func (sa *safeArena) getPOD(ty reflect.Type, n int) unsafe.Pointer {
+	return sa.podSlabs.getPOD(ty, n)
 }
 
 type safeSlabGroup struct {
@@ -166,33 +101,58 @@ type safeSlabGroup struct {
 	firstWithFreeSpace int
 }
 
-func makeSafeSlabGroup[T any](initialSlots int) safeSlabGroup {
-	var t T
+func makeSafeSlabGroup(ty reflect.Type, initialSlots int) safeSlabGroup {
+	firstSlab := makeSafeSlab(ty, initialSlots)
 	return safeSlabGroup{
-		slabs:              []safeSlab{makeSafeSlab[T](initialSlots)},
-		totalBytes:         initialSlots * int(unsafe.Sizeof(t)),
+		slabs:              []safeSlab{firstSlab},
+		totalBytes:         firstSlab.size,
 		firstWithFreeSpace: 0,
 	}
 }
 
-func (sg *safeSlabGroup) getFast(size uintptr) unsafe.Pointer {
-	for i := sg.firstWithFreeSpace; i < len(sg.slabs); i++ {
-		ptr := sg.slabs[i].getFast(size)
-		if ptr != nil {
-			return ptr
-		}
-		if sg.slabs[i].seemsFull() {
-			// Swap this "seems full now" slab to the front of the range of
-			// slabs with free space
-			sg.slabs[sg.firstWithFreeSpace], sg.slabs[i] = sg.slabs[i], sg.slabs[sg.firstWithFreeSpace]
-			// Don't try to allocate in that slab any more, it seems full
-			sg.firstWithFreeSpace++
+// Allocates a slice in the slab group with the given type and returns the
+// pointer to its front. This will grow the slab group as necessary.
+//
+// Only call this method on a typed slab group! It will allocate slabs as []ty,
+// and they will only be suitable for that type.
+func (sg *safeSlabGroup) getTyped(ty reflect.Type, n int) unsafe.Pointer {
+	ptr := sg.getAlwaysAligned(ty.Size() * uintptr(n))
+	if ptr == nil {
+		sg.grow(ty, n)
+		ptr = sg.getAlwaysAligned(ty.Size() * uintptr(n))
+		if ptr == nil {
+			// This should never happen
+			panic("slab allocation failed!")
 		}
 	}
-	return nil // No space found, slab group needs to grow
+	return ptr
 }
 
-func (sg *safeSlabGroup) get(size uintptr, align uintptr) unsafe.Pointer {
+// Allocates a slice in the slab group with the given type and returns the
+// pointer to its front. This will grow the slab group as necessary.
+//
+// Only call this method on a POD slab group! It will allocate slabs as []byte,
+// and they will only be suitable for POD values.
+func (sg *safeSlabGroup) getPOD(ty reflect.Type, n int) unsafe.Pointer {
+	size, align := ty.Size(), ty.Align()
+	ptr := sg.getWithAlignment(size, uintptr(align))
+	if ptr == nil {
+		// When making new slabs for POD types, we need to make sure there is
+		// at least space for n+1 (unaligned) values of T in case the new
+		// slab is not sufficiently aligned for T; otherwise we could end up
+		// being unable to fit the value in the new slab after correcting
+		// alignment.
+		sg.grow(byteType, (n+1)*int(size))
+		ptr = sg.getWithAlignment(size, uintptr(align))
+		if ptr == nil {
+			// This should never happen
+			panic("slab allocation failed!")
+		}
+	}
+	return ptr
+}
+
+func (sg *safeSlabGroup) getWithAlignment(size uintptr, align uintptr) unsafe.Pointer {
 	for i := sg.firstWithFreeSpace; i < len(sg.slabs); i++ {
 		ptr := sg.slabs[i].get(size, align)
 		if ptr != nil {
@@ -210,27 +170,23 @@ func (sg *safeSlabGroup) get(size uintptr, align uintptr) unsafe.Pointer {
 	return nil // No space found, slab group needs to grow
 }
 
-// Allocates a slice in the slab group with the given layout and returns the
-// pointer to its front. This will grow the slab group as necessary.
-//
-// Only call this method on a POD slab group! It will allocate slabs as []byte,
-// and they will only be suitable for POD values.
-func (sg *safeSlabGroup) newPODSlice(tLayout layout, n int) unsafe.Pointer {
-	ptr := sg.get(tLayout.size, tLayout.align)
-	if ptr == nil {
-		// When making slots for POD types, we need to make sure there is
-		// at least space for n+1 (unaligned) values of T in case the new
-		// slab is not sufficiently aligned for T; otherwise we could end up
-		// being unable to fit the value in the new slab after correcting
-		// alignment.
-		growSlabGroup[byte](sg, 1, (n+1)*int(tLayout.size))
-		ptr = sg.getFast(tLayout.size)
-		if ptr == nil {
-			// This should never happen
-			panic("slab allocation failed!")
+// Gets a pointer to a value by a trivial bump. Suitable only for alignment=1
+// values and values in slabs that are allocated as []T and only ever contain T.
+func (sg *safeSlabGroup) getAlwaysAligned(size uintptr) unsafe.Pointer {
+	for i := sg.firstWithFreeSpace; i < len(sg.slabs); i++ {
+		ptr := sg.slabs[i].getFast(size)
+		if ptr != nil {
+			return ptr
+		}
+		if sg.slabs[i].seemsFull() {
+			// Swap this "seems full now" slab to the front of the range of
+			// slabs with free space
+			sg.slabs[sg.firstWithFreeSpace], sg.slabs[i] = sg.slabs[i], sg.slabs[sg.firstWithFreeSpace]
+			// Don't try to allocate in that slab any more, it seems full
+			sg.firstWithFreeSpace++
 		}
 	}
-	return ptr
+	return nil // No space found, slab group needs to grow
 }
 
 func (sg *safeSlabGroup) reset() {
@@ -251,13 +207,15 @@ func (sg *safeSlabGroup) reset() {
 	}
 }
 
-func growSlabGroup[T any](sg *safeSlabGroup, tSize uintptr, newSlots int) {
-	currentTotalSlots := sg.totalBytes / int(tSize)
-	if currentTotalSlots > newSlots {
+func (sg *safeSlabGroup) grow(ty reflect.Type, minNewSlots int) {
+	currentTotalSlots := sg.totalBytes / int(ty.Size())
+	newSlots := minNewSlots
+	if currentTotalSlots > minNewSlots {
+		// At minimum we double the total capacity of the slab group
 		newSlots = currentTotalSlots
 	}
-	sg.slabs = append(sg.slabs, makeSafeSlab[T](newSlots))
-	sg.totalBytes += newSlots * int(tSize)
+	sg.slabs = append(sg.slabs, makeSafeSlab(ty, newSlots))
+	sg.totalBytes += newSlots * int(ty.Size())
 }
 
 type safeSlab struct {
@@ -266,14 +224,15 @@ type safeSlab struct {
 	offset uintptr
 }
 
-func makeSafeSlab[T any](slots int) safeSlab {
-	var t T
+func makeSafeSlab(ty reflect.Type, slots int) safeSlab {
+	// We can still retain ownership of the buffer by punning it as another
+	// type, and we can still bump-allocate the precise typed slots of the slice
+	// if we only ever put that specific type in it; the buffer is therefore
+	// owned as an unsafe.Pointer to the first element of the slice, and size is
+	// the number of bytes in that slice.
 	return safeSlab{
-		// We can still retain ownership of the buffer by punning it as another
-		// type, and we can still bump-allocate the precise typed slots of the
-		// slice if we only ever put that specific type in it.
-		buf:    unsafe.Pointer(&make([]T, slots)[0]),
-		size:   slots * int(unsafe.Sizeof(t)),
+		buf:    reflect.MakeSlice(ty, slots, slots).UnsafePointer(),
+		size:   slots * int(ty.Size()),
 		offset: 0,
 	}
 }
@@ -287,13 +246,13 @@ func (s *safeSlab) getFast(size uintptr) unsafe.Pointer {
 	return newPointer
 }
 
-func (s *safeSlab) get(size uintptr, alignment uintptr) unsafe.Pointer {
+func (s *safeSlab) get(size uintptr, align uintptr) unsafe.Pointer {
 	newOffset := s.offset + size
 	var realign uintptr = 0
-	if alignment != 1 {
-		misalignment := (uintptr(s.buf) + newOffset) % alignment
+	if align != 1 {
+		misalignment := (uintptr(s.buf) + newOffset) % align
 		if misalignment != 0 {
-			realign = alignment - misalignment
+			realign = align - misalignment
 		}
 		newOffset += realign
 	}
